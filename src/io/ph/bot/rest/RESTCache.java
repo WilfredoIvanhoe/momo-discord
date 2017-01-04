5f77b6c11f7d5fe69d3828080c830568a471987b
Paul Hamill <paul.hamill@gatech.edu>
2017-01-04 04:46:35 -0500

    refactor RESTCache
    Multiple interceptors that may need this cache timer

--- src/io/ph/bot/rest/RESTCache.java
+++ src/io/ph/bot/rest/RESTCache.java
@@ -11,13 +11,13 @@ import okhttp3.Response;
 public class RESTCache {
 	public static OkHttpClient client;
 	private static final File cacheDir = new File("resources/cache/");
-	
+	public static final int CACHE_AGE = 60 * 60 * 24 * 7;
 	private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
 		@Override public Response intercept(Chain chain) throws IOException {
 			Response originalResponse = chain.proceed(chain.request());
-			int maxAge = 60 * 60 * 24 * 7;
+			
 			return originalResponse.newBuilder()
-					.header("Cache-Control", "public, max-age=" + maxAge)
+					.header("Cache-Control", "public, max-age=" + CACHE_AGE)
 					.build();
 		}
 	};
